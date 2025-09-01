0c82848346418d788bea619a05c68138a7349274
yangxucs1 <35906909+yangxucs1@users.noreply.github.com>
2025-09-01 21:07:24 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #feeds添加kenzok8的软件包仓库
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 #echo "packages executed successfully!"
