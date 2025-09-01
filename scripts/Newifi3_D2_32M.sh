6e798a007ecb43011d3cc5f65a876ed45589e7be
yangxucs1 <35906909+yangxucs1@users.noreply.github.com>
2025-09-01 16:22:15 +0800

    Add files via upload

---
+++ scripts/Newifi3_D2_32M.sh
@@ -0,0 +1,2 @@
+#cd openwrt
+#sed -i '/PARALLEL/a PKG_USE_MIPS16:=0' feeds/smpackage/dns2tcp/Makefile
---
+++ scripts/TL-WDR7500_V3_16M.sh
@@ -0,0 +1,5 @@
+##### TL-WDR7500_V3 修改编译文件16M #####
+sed -i 's/tplink-4mlzma/tplink-16mlzma/g' target/linux/ath79/image/generic-tp-link.mk
+sed -i 's/<0x020000 0x7d0000>/<0x020000 0xfd0000>/g' target/linux/ath79/dts/qca9558_tplink_tl-wdr7500-v3.dts
+sed -i 's/partition@7f0000/partition@ff0000/g' target/linux/ath79/dts/qca9558_tplink_tl-wdr7500-v3.dts
+sed -i 's/<0x7f0000 0x010000>/<0xff0000 0x010000>/g' target/linux/ath79/dts/qca9558_tplink_tl-wdr7500-v3.dts
